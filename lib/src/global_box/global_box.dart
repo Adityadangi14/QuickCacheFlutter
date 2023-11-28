8e0fb37f05e95915e675a6cd7c8394294fc93b70
Adityadangi14 <dangiaditya@gmail.com>
2023-11-28 14:51:39 +0530

    added path provider

--- lib/src/global_box/global_box.dart
+++ lib/src/global_box/global_box.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
 class GlobalBox {
   GlobalBox._();
@@ -9,6 +11,8 @@ class GlobalBox {
   static final instance = GlobalBox._();
   final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
   Future<Box> getGlobalBox() async {
+    Directory dir = await getTemporaryDirectory();
+    Hive.init(dir.path);
     var encryptionKey =
         base64Url.decode(await secureStorage.read(key: 'encryptionKey') ?? '');
 
