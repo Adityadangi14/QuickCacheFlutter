05303e6ac6980e1681b7fc6333eed508c0abf10e
Adityadangi14 <dangiaditya@gmail.com>
2023-11-28 12:11:37 +0530

    version updated and bug fixed

--- lib/src/encryption/get_encrypt_key_repo_impl.dart
+++ lib/src/encryption/get_encrypt_key_repo_impl.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hive/hive.dart';
-import 'package:logger/logger.dart';
 import 'package:quick_cache_flutter/src/encryption/get_encryt_key_repo.dart';
 
 class GetEncryptKeyRepoImpl implements GetEncryptKeyRepo {
@@ -15,13 +14,9 @@ class GetEncryptKeyRepoImpl implements GetEncryptKeyRepo {
     final FlutterSecureStorage secureStorage = FlutterSecureStorage(
       aOptions: _getAndroidOptions(),
     );
-    var logger = Logger(
-      printer: PrettyPrinter(),
-    );
     var containsEncryptionKey = await secureStorage.read(key: 'encryptionKey');
 
-    if (containsEncryptionKey != null) {
-      logger.i(containsEncryptionKey);
+    if (containsEncryptionKey == null) {
       var key = Hive.generateSecureKey();
       await secureStorage.write(key: 'encryptionKey', value: base64Encode(key));
     }
